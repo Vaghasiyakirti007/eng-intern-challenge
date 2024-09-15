3924a292a8c1151dab0cb7db2099a04fa6da08ab
Vaghasiyakirti007 <kirtilifekirti007@gmail.com>
2024-09-15 15:44:04 -0500

    Add files via upload
    Hello,
    
    i hope you are doing well, I solved this challenge using javascript, and added ruby and python program as well.
    
    Best regards,
    Kirti vaghasiya,
    +1 226-966-6045

---
+++ eng-intern-challenge-main/eng-intern-challenge-main/ruby/translator.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+braille_alphabet = {
+  'a' => 'O.....', 'b' => 'O.O...', 'c' => 'OO....', 'd' => 'OO.O..', 'e' => 'O.OO..',
+  'f' => 'OOO...', 'g' => 'OOOO..', 'h' => 'O.OO..', 'i' => '.OO...', 'j' => '.OOO..',
+  'k' => 'O...O.', 'l' => 'O.O.O.', 'm' => 'OO..O.', 'n' => 'OO.OO.', 'o' => 'O.OOO.',
+  'p' => 'OOO.O.', 'q' => 'OOOOO.', 'r' => 'O.OOO.', 's' => '.OO.O.', 't' => '.OOO.',
+  'u' => 'O...OO', 'v' => 'O.O.OO', 'w' => '.OOO..', 'x' => 'OO..OO', 'y' => 'OO.OOO',
+  'z' => 'O.OOOO', ' ' => '......', '0' => '.O....', '1' => 'O.....', '2' => 'O.O...',
+  '3' => 'OO....', '4' => 'OO.O..', '5' => 'O.OO..', '6' => 'OOO...', '7' => 'OOOO..',
+  '8' => 'O.OO..', '9' => '.OO...'
+}
+
+braille_to_english = {
+  'O.....' => 'a', 'O.O...' => 'b', 'OO....' => 'c', 'OO.O..' => 'd', 'O.OO..' => 'e',
+  'OOO...' => 'f', 'OOOO..' => 'g', 'O.OO..' => 'h', '.OO...' => 'i', '.OOO..' => 'j',
+  'O...O.' => 'k', 'O.O.O.' => 'l', 'OO..O.' => 'm', 'OO.OO.' => 'n', 'O.OOO.' => 'o',
+  'OOO.O.' => 'p', 'OOOOO.' => 'q', 'O.OOO.' => 'r', '.OO.O.' => 's', '.OOO.' => 't',
+  'O...OO' => 'u', 'O.O.OO' => 'v', '.OOO..' => 'w', 'OO..OO' => 'x', 'OO.OOO' => 'y',
+  'O.OOOO' => 'z', '......' => ' ', '.O....' => '0', 'O.....' => '1', 'O.O...' => '2',
+  'OO....' => '3', 'OO.O..' => '4', 'O.OO..' => '5', 'OOO...' => '6', 'OOOO..' => '7',
+  'O.OO..' => '8', '.OO...' => '9'
+}
+
+def braille_to_english(braille)
+  braille.split.map { |b| braille_to_english[b] || '?' }.join
+end
+
+def english_to_braille(text)
+  text.downcase.chars.map { |c| braille_alphabet[c] || '?' }.join(' ')
+end
+
+input = ARGV.join(' ')
+is_braille = input.chars.all? { |c| 'O. '.include?(c) }
+
+result = is_braille ? braille_to_english(input) : english_to_braille(input)
+puts result
